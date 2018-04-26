db92e9de1a25a55a050b4edf6bd2cb895815bd0c
Hadar Eliyahu <hadar.eliyahu@mail.huji.ac.il>
2018-04-26 20:12:27 +0300

    creating object Word

--- app/src/main/java/beckend/Syllable.java
+++ app/src/main/java/beckend/Syllable.java
@@ -1,7 +1,20 @@
 package beckend;
 
 public class Syllable {
-    String key;
-    String representation;
-    String audioPath;
+    private String key;
+    private String representation;
+    private String audioPath;
+    public Syllable(String key, String rep) {
+        this.key = key;
+        this.representation = rep;
+    }
+    public String toString(){
+        return representation;
+    }
+    public String getKey(){
+        return key;
+    }
+    public String getAudioPath(){
+        return audioPath;
+    }
 }
