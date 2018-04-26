baf36d43a34764264d75875ec23354a793123ba8
elazar.cohen1 <aueuks>
2018-04-26 22:18:26 +0300

    add voice files

--- app/src/test/java/backend/RandomizerImplementTest.java
+++ app/src/test/java/backend/RandomizerImplementTest.java
@@ -2,6 +2,8 @@ package backend;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class RandomizerImplementTest {
@@ -13,9 +15,22 @@ public class RandomizerImplementTest {
 
     @Test
     public void makeSyllable() {
+        Syllable s;
+        for (int i = 0; i < 100; i++) {
+
+            s= t.makeSyllable(false,false);
+                System.out.println(s.getKey());
+        }
     }
 
     @Test
     public void getWord() {
+        Word s;
+        for (int i = 0; i < 10000; i++) {
+
+            s= t.getWord();
+            if (s.toString().contains("null"))
+                System.out.println(s);
+        }
     }
 }
