0adb1b8390cbcef254a4693d31da111a87b875dc
elazar.cohen1 <aueuks>
2018-04-26 21:10:47 +0300

    rename backend

---
+++ app/src/test/java/backend/RandomizerImplementTest.java
@@ -0,0 +1,21 @@
+package backend;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RandomizerImplementTest {
+    RandomizerImplement t = new RandomizerImplement();
+    @Test
+    public void getNextSymbol() {
+        System.out.println(t.getNextSymbol());
+    }
+
+    @Test
+    public void makeSyllable() {
+    }
+
+    @Test
+    public void getWord() {
+    }
+}
