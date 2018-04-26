302b6a25d878aac13b7168847d8b0794779cb1da
eli-shapira <eli53466@gmail.com>
2018-04-26 22:31:47 +0300

    make new activity

---
+++ app/src/main/java/shellybekhor/dexi/GameWordActivity.java
@@ -0,0 +1,16 @@
+package shellybekhor.dexi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class GameWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_word);
+
+    }
+}
--- app/src/main/java/shellybekhor/dexi/MainActivity.java
+++ app/src/main/java/shellybekhor/dexi/MainActivity.java
@@ -22,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void moveToGameWords(View view) {
+        Intent intent = new Intent(MainActivity.this, GameWordActivity.class);
+        startActivity(intent);
+    }
+
 
 
     public void audioPlayer(View view){
